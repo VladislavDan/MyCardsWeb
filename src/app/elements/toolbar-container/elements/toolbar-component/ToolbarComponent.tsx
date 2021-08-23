8c9f51c18b218be1d0bc271917e95fb326ce4ff6
Vladislav Danilov <Vladislav_Danilov@epam.com>
2021-08-23 22:16:34 +0400

    Added repeat icon

--- src/app/elements/toolbar-container/elements/toolbar-component/ToolbarComponent.tsx
+++ src/app/elements/toolbar-container/elements/toolbar-component/ToolbarComponent.tsx
@@ -1,18 +1,25 @@
 import {FC, MouseEventHandler} from 'react';
 import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
+import './ToolbarComponent.css'
+import {Routs} from '../../../../common/Routs';
 
 export const ToolbarComponent: FC<IToolbarComponent> = ({pageLabel, onClick}) => {
 
     return (
         <AppBar position="fixed">
-            <Toolbar>
-                <IconButton edge="start" color="inherit" aria-label="menu" onClick={onClick}>
+            <Toolbar className="toolbar">
+                <IconButton className="toolbar_menu-icon" edge="start" color="inherit" aria-label="menu" onClick={onClick}>
                     <MenuIcon/>
                 </IconButton>
-                <Typography variant="h6">
+                <Typography className="toolbar_label" variant="h6">
                     {pageLabel}
                 </Typography>
+                {pageLabel === Routs.cards.name || pageLabel === Routs.cardsGroups.name ? <IconButton color="inherit">
+                    <PlayArrowIcon/>
+                </IconButton> : null}
             </Toolbar>
         </AppBar>
     )
